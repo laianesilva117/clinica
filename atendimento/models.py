cf95993a69c49677eb9e58f46948bdf83ddedc70
Your Name <you@example.com>
2019-05-30 20:10:25 -0300

    Cria um modelo medicamento

--- atendimento/models.py
+++ atendimento/models.py
@@ -11,3 +11,8 @@ class Medico (models.Model):
     nome= models.CharField (max_length=50)
     especialidade= models.CharField(max_length=50)
     idade= models.IntegerField()
+
+class Medicamento(models.Model):
+    nome= models.CharField(max_length=50)
+    valor= models.DecimalField(max_digits= 30, decimal_places=2)
+    gravida= models.BooleanField()
