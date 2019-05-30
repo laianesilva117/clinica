2c44efbd466756c2919485f4de71f9b4fe3ffc4d
Your Name <you@example.com>
2019-05-30 19:40:27 -0300

    Cria Modelo Médico

--- atendimento/admin.py
+++ atendimento/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from . models import Paciente
+from . models import Medico
 
 admin.site.register(Paciente)
+admin.site.register(Medico)
 # Register your models here.
